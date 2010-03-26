aef126f5e699580fc8c52bd478f45565fb2d8eb0
mario manzoni <mariomanzoni@kubepartners.com>
2010-03-26 19:35:09 +0100

    changes for bash script utils

--- code/crawler.sh
+++ code/crawler.sh
@@ -1,5 +1,15 @@
-cd /home/notroot/git/KTE/code/run_time/rake/tasks
-rake rc:restart
-cd /home/notroot/git/KTE/code/run_time/crawlers/script
-ruby run.rb /home/notroot/git/KTE/code/yamls/prov_1_sect_1_dialogo.yml
+#!/bin/bash
+if [ $1 -eq  ]; then
+	echo "Pass the yaml file name on command line argument (ie. ./crawler.sh quixa.yml)"
+else 
+	echo @@@ $1 @@@
+	echo "Starting ..."
+	yml="/home/notroot/git/KTE/code/yamls/prov_1_sect_1_"$1
+	cd /home/notroot/git/KTE/code/run_time/rake/tasks
+	rake rc:restart
+	cd /home/notroot/git/KTE/code/run_time/crawlers/script
+	echo $yml
+	ruby run.rb $yml
+	echo "Finished!" */
+fi 
 
