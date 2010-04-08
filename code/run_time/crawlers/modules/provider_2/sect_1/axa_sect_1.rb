de3e371076c2141a95b81bdcd907e0df20c198c3
fabrizio licitra <fabriziolicitra@kubepartners.com>
2010-04-08 09:48:24 +0200

    provider2 sector1 axa runtime etl

--- code/run_time/crawlers/modules/provider_2/sect_1/axa_sect_1.rb
+++ code/run_time/crawlers/modules/provider_2/sect_1/axa_sect_1.rb
@@ -75,10 +75,6 @@ class AxaSect1 < Test::Unit::TestCase
 
       page_intro
       page_1
-      page_2
-      page_3
-      page_4
-      page_5
       page_premium
 
       @kte.test_result = "Test OK => New RCA price for profile [#{@kte.profile}] and record [#{@record}]: € #{@kte.rc_premium}"
@@ -146,161 +142,31 @@ class AxaSect1 < Test::Unit::TestCase
     select_option "TIPTGA", get('@number_plate_type')
     
     select_option "MARCA", get("@make")
-    sleep @sleep*2
+    page_wait
     select_option "ALLEST", get("@model")
-    sleep @sleep*2
+    page_wait
 
-    click_button '//*input[@value="Avanti"]'
+    if is_present?("HP")
+      select_option "HP", get("@cv")
+    end
 
     select_option "PPFRAZ", get("@instalment")
     select_option "*MRCA", get('@public_liability_indemnity_limit')
     select_option "AXABM", get("@bm_assigned")
-    select_option "FTARIF", get("@type_of_contract")
+    select_option "FTARIF", get("@quotation")
 
     click_button 'clausole'
+    page_wait
 
-    click_option(get('@renounce_compensation'))
-    click_option(get('@exclusive_drive'))
-    click_option(get('@defined_drive'))
-    select_option "*FRCA", get("@public_liability_exemptio")
+#    click_option(get('@renounce_compensation'))
+#    click_option(get('@exclusive_drive'))
+#    click_option(get('@defined_drive'))
+#    select_option "*FRCA", get("@public_liability_exemption")
 
     click_button 'btnCalcolaPremio'
     page_wait
     
   end
-
-#  def page_2
-#
-#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-#
-#
-#    select_option "page:mese_immatricolazione", get("@matriculation_date_month")
-#    type_text("page:anno_immatricolazione", get("@matriculation_date_year"))
-#    type_text("page:anno_acquisto", get("@purchase_date_year"))
-#    select_option "page:alimentazione", get("@fuel")
-#
-#    select_option "page:allestimento", get("@set_up")
-#
-#    click_button 'page:continua_step01'
-#
-#    type_text("page:valore_veicolo", get('@vehicle_value'))
-#    page.fire_event 'page:valore_veicolo', 'blur'
-#    select_option "page:ricovero_notturno", get("@vehicle_shelter")
-#    select_option "page:antifurto", get("@alarm")
-#    select_option "page:uso_prevalente", get("@habitual_vehicle_use")
-#    select_option "page:km_anno", get("@km_per_yr")
-#    click_option(get('@modification_made'))
-#    click_option(get('@bersani'))
-#    click_option(get('@subscriber_is_owner'))
-#    if (page.get_attribute("#{@last_element}@value") == "0")
-#      click_option(get('@client_type'))
-#    end
-#    click_option(get('@subscriber_is_driver'))
-#
-#    click_button 'page:buttonContinua'
-#   	page_wait
-#
-#  end
-#
-#  def page_3
-#
-#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-#
-#    click_option()
-#    click_option(get('@citizenship'))
-#    if get('@citizenship') == "page:conducente_nazione:0" #ITALIA
-#      select_option "page:provincia_di_nascita", get("@birth_province")
-#      type_keys("page:comune_di_nascita", get('@birth_place'))
-#      sleep @sleep*2
-#      page.click "//div[@id='risultatiSrchComNas']/ul/span/li"
-#    else #ESTERO
-#      select_option "page:conducente_principale_nazione_estera", get("@birth_state")
-#      type_text("page:anno_residenza_italia", get('@italian_residence_starting_yrs'))
-#    end
-#
-#    click_button 'page:buttonContinua'
-#
-#    select_option "page:provincia_di_residenza", get("@residence_province")
-#    type_keys("page:comune_di_residenza", get('@residence'))
-#    sleep @sleep*2
-#    page.click "//div[@id='risultatiSrchComRes']/ul/span/li"
-#
-#    type_text("page:toponimo_residenza", get('@toponym'))
-#    type_text("page:indirizzo_residenza", get('@address_street'))
-#    type_text("page:numero_residenza", get('@address_num'))
-#    page.click 'page:cap_di_residenza'
-#    select_option "page:cap_di_residenza", get("@owner_zip_code")
-#
-#    click_button 'page:buttonContinua2'
-#
-#    type_keys("page:professione_conducente_principale", get('@job'))
-#    sleep @sleep*2
-#    page.click "//div[@id='risultatiSrchProf']/ul/span/li"
-#
-#    select_option "page:stato_civile", get("@civil_status")
-#    click_option(get('@cohabiting_children'))
-#    type_text("page:eta_conseguimento_patente", get('@driving_license_yrs'))
-#    page.fire_event("page:eta_conseguimento_patente", 'blur')
-#    if is_present?("page:anno_conseguimento_patente")
-#      type_text("page:anno_conseguimento_patente", get('@driving_license_year_of_issue'))
-#      select_option "page:mese_conseguimento_patente", get("@driving_license_month_of_issue")
-#    end
-#    select_option "page:tipo_patente", get("@driving_license_type")
-#    select_option "page:punti_patente", get("@driving_license_points")
-#
-#    click_button 'page:buttonContinua3'
-#    page_wait
-#
-#  end
-#
-#  def page_4
-#
-#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-#
-#    click_option(get('@drunkenness_fine'))
-#    click_option(get('@driving_license_suspension'))
-#    click_option(get('@other_vehicle_use'))
-#
-#    click_button '//*[@class="continua_button"]/table/tbody/tr/td/a/img'#'page:buttonContinua6'
-#
-#    click_option(get('@claims_total_number'))
-#    if (page.get_attribute("#{@last_element}@value") == "1")
-#      select_option "page:numero_sinistri_cc", get("@nr_of_paid_claims_2_yr")
-#      select_option "page:MeseSinistroConColpa1", get("@first_claim_month")
-#      select_option "page:AnnoSinistroConColpa1", get("@first_claim_year")
-#
-#      if get("@nr_of_paid_claims_2_yr") == '2'
-#        select_option "page:MeseSinistroConColpa2", get("@second_claim_month")
-#        select_option "page:AnnoSinistroConColpa2", get("@second_claim_year")
-#      end
-#
-#    end
-#
-#    click_button 'page:buttonContinua14'
-#
-#    type_text("page:cognome_contraente", get('@name'))
-#    type_text("page:nome_contraente", get('@surname'))
-#    type_text("page:email_contraente", get('@e_mail'))
-#    type_text("page:prefisso_cellulare_contraente", get('@mobile_prefix'))
-#    type_text("page:cellulare_contraente", get('@mobile_number'))
-#
-#    click_button 'page:buttonContinua3'
-#    page_wait
-#
-#  end
-#
-#  def page_5
-#
-#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-#
-#    click_option(get('@driving_type'))
-#    click_option(get('@payment'))
-#    click_option('page:metodoDiPagamento2:0')
-#
-#    click_button 'page:buttonCalcolaPremioAppoggio'
-#    page_wait
-#
-#  end
 
   def page_premium
 
@@ -308,26 +174,6 @@ class AxaSect1 < Test::Unit::TestCase
     @last_element, @last_value = "@rca_on_off", get("@rca_on_off")
     case @last_value
       when 'on'
-#        sleep @sleep*2
-#        select_option("page:quota_info:0:massimaleRCA", get('@public_liability_indemnity_limit'))
-#
-#        uncheck_checkbox(get('@assistance_web_id')) if is_checked?(get('@assistance_web_id'))
-#        uncheck_checkbox(get('@legal_assistance_web_id')) if is_checked?(get('@legal_assistance_web_id'))
-#        uncheck_checkbox(get('@driver_accident_coverage_web_id')) if is_checked?(get('@driver_accident_coverage_web_id'))
-#        uncheck_checkbox(get('@road_assistance_web_id')) if is_checked?(get('@road_assistance_web_id'))
-#        uncheck_checkbox(get('@contingency_protection_web_id')) if is_checked?(get('@contingency_protection_web_id'))
-#        uncheck_checkbox(get('@glasses_web_id')) if is_checked?(get('@glasses_web_id'))
-#        uncheck_checkbox(get('@kasko_web_id')) if is_checked?(get('@kasko_web_id'))
-#        uncheck_checkbox(get('@natural_events_act_of_vandalism_web_id')) if is_checked?(get('@natural_events_act_of_vandalism_web_id'))
-#        uncheck_checkbox(get('@theft_fire_coverage_web_id')) if is_checked?(get('@theft_fire_coverage_web_id'))
-#
-#        page.fire_event 'page:quota_info:0:massimaleRCA', 'change'
-#        sleep @sleep*4
-#        wait_for_elm('page:ricalcola_but')
-#        page_click_button 'page:ricalcola_but'
-#
-#        page_wait
-
         @last_element, @last_value = "@rca_premium_id", get("@rca_premium_id")
         wait_for_elm @last_value
 
@@ -478,13 +324,11 @@ class AxaSect1 < Test::Unit::TestCase
 
     @last_element = p
     premium = page.get_text(@last_element)
-#    assert premium.split[0] != nil, @last_element.inspect
-#    assert premium.split[0].to_s.match(/[a-zA-Z]/) == nil, @last_element.inspect
-#    premium = premium.split[1].gsub(".","")
-#    premium = premium.gsub(",",".")
+    premium = premium.gsub(".","")
+    premium = premium.gsub(",",".")
 
     @logger.debug("#{__FILE__} => #{method_name}") {"#{@kte.company} => PREMIUM = € #{premium.to_s}"}
-#    assert_not_equal 0, premium.to_i, "Price cannot be equal to zero"
+    assert_not_equal 0, premium.to_i, "Price cannot be equal to zero"
     @kte.rc_premium = premium
 
   end
