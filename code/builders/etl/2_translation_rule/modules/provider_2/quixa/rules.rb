27febdfad86c1461ece9a4fa1edb9035ca5098ef
silvia zamperlin <silviazamperlin@kubepartners.com>
2010-04-08 11:05:22 +0200

    fixes

--- code/builders/etl/2_translation_rule/modules/provider_2/quixa/rules.rb
+++ code/builders/etl/2_translation_rule/modules/provider_2/quixa/rules.rb
@@ -32,7 +32,7 @@ module Provider2Quixa
       'own_owner_sex_str' => 'translate_field',
       'own_owner_specification_str' => 'copy_field',
       'own_owner_zip_code_str' => 'copy_field',
-      'own_owner_residence_province_str' =>  'translate_field'   ,
+      'own_owner_residence_province_str' =>  'copy_field'   ,
       'own_owner_residence_str' =>  'copy_field',
       'pol_act_of_vandalism_code_str' => 'copy_field',
       'pol_act_of_vandalism_minimum_uncoverage_str' => 'copy_field',
