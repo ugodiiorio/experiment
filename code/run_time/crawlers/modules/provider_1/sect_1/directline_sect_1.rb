ce7f3a96dda47356e95edde321dae3458faf0158
fabrizio licitra <fabriziolicitra@kubepartners.com>
2010-04-26 18:02:43 +0200

    provider1 sector1 directline runtime etl

--- code/run_time/crawlers/modules/provider_1/sect_1/directline_sect_1.rb
+++ code/run_time/crawlers/modules/provider_1/sect_1/directline_sect_1.rb
@@ -1,7 +1,10 @@
 #############################################
 #   	Created by Kubepartners			          #
 #                                           #
-#				19/03/2010						              #
+#				19/03/2010                          #
+#					                                  #
+#     modified: 26/04/2010                  #
+#                         	                #
 #############################################
 
 class DirectlineSect1 < Test::Unit::TestCase
@@ -109,7 +112,7 @@ class DirectlineSect1 < Test::Unit::TestCase
   def page_intro
 
     open_page(@url)
-    click_button "//img[@src='/images/bottone_lancio_preventivo-auto.gif']"
+    click_button "//img[@alt='Per la tua auto']"
    	page_wait
 
   end
@@ -119,14 +122,37 @@ class DirectlineSect1 < Test::Unit::TestCase
     @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
     @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
 
-    sleep @sleep*2
-    click_option(get('@insurance_situation'))
-    (page.get_attribute("#{@last_element}@value") == "XX") ? click_option(get('@already_benefit_from_bersani')) : nil
-    type_text("dataInizioValidita", @rate_date)
-#    type_text("Targa", get('@bersani_ref_vehicle_number_plate'))
-    select_option "ConoscenzaAllState", get("@how_do_you_know_the_company")
+#    sleep @sleep*2
+    click_option(get('@privacy_1'))
+    click_button "//img[@id='btnPrivacy']"
 
-    click_button "SUBMIT"
+    page.click(get('@insurance_situation'))
+
+    if get('@insurance_situation') =~ /bg_radio1/i
+      click_option(get('@already_benefit_from_bersani'))
+
+      type_text("giornoDecorrenzaPolizza", @rate_date.slice(0,2))
+      type_text("meseDecorrenzaPolizza", @rate_date.slice(3,2))
+      type_text("annoDecorrenzaPolizza", @rate_date.slice(6,4))
+
+      page.click('classeBMProvenienza_link')
+      page.click(get("@coming_from_bm"))
+      page.click('classeBMAssegnazione_link')
+      page.click(get("@bm_assigned"))
+      
+      click_option(get('@claims_total_number'))
+      click_option(get('@nr_of_yrs_insured_in_the_last_5_yrs'))
+
+    else
+
+
+    end
+
+
+#    type_text("Targa", get('@bersani_ref_vehicle_number_plate'))
+#    select_option "ConoscenzaAllState", get("@how_do_you_know_the_company")
+
+    click_button "btnContinua"
    	page_wait
 
   end
@@ -136,35 +162,37 @@ class DirectlineSect1 < Test::Unit::TestCase
     @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
     @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
 
-    type_text("iddatadinascita", get('@birth_date'))
+    type_text("policyHolder_bornDay", get('@birth_date_day'))
+    type_text("policyHolder_bornMonth", get('@birth_date_month'))
+    type_text("policyHolder_bornYear", get('@birth_date_year'))
+
     click_option(get('@owner_sex'))
-    select_option "idstatocivile", get("@civil_status")
-    select_option "/qol/application/beans/vo/VoContraente.strCodProfessione", get("@job")
-    store_parameter(:job, page.get_selected_label(@last_element)) if @store_params
-    type_text("CAP", get('@owner_zip_code'))
-    select_option "/qol/application/beans/vo/VoContraente.strAnniPatente", get("@driving_license_yrs")
+
+    page.click('policyHolder_maritalStatus_link')
+    page.click(get("@civil_status"))
+
+    page.click('policyHolder_profession_link')
+    page.click(get("@job"))
+
+#    store_parameter(:job, page.get_selected_label(@last_element)) if @store_params
+    type_text("policyHolder_zipCode", get('@owner_zip_code'))
+#    type_text("policyHolder_town_link", get('@residence')) it's auto-compiled so we can comment this line
+
+    page.click('policyHolder_drivingLicencePossess_link')
+    page.click(get("@driving_license_yrs"))
+    
     click_option(get('@subscriber_is_driver'))
     click_option(get('@subscriber_is_owner'))
 
-    click_button "SUBMIT"
+    page.click('numeroConducentiMinori26Anni_link')
+    page.click(get("@driver_less_than_26_yrs"))
+    
+    if get("@driver_less_than_26_yrs") == 'numeroConducentiMinori26Anni_li_2'
+      page_more_drivers
+    end
+
+    click_button "btnContinua"
    	page_wait
-
-  end
-
-  def page_3
-
-    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
-
-    select_option "numConducentiU26Dich", get("@driver_less_than_26_yrs")
-    @last_value =~ /un conducente/i ? @one_driver_young = true : @one_driver_young = false
-    type_text("Numauto", get('@family_car'))
-    click_option(get('@family_members_insured_with_company'))
-
-    click_button "SUBMIT"
-   	page_wait
-
-    page_more_drivers if @one_driver_young
 
   end
 
@@ -173,15 +201,58 @@ class DirectlineSect1 < Test::Unit::TestCase
     @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
     @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
 
-    driver_birth_date = format_date(Chronic.parse("25 year ago tomorrow"))
-    type_text("dataNascitaConducenteUno", driver_birth_date)
-    click_option "//input[@name='sessoConducenteUno' and @value='#{get("@owner_specification")}']"
-    select_option "/qol/application/beans/vo/VoConducenteUno.strCodStatoCivile", get("@civil_status")
-    select_option "/qol/application/beans/vo/VoConducenteUno.strCodProfessione", get("@job")
-    type_text("CAP", get('@owner_zip_code'))
-    select_option "/qol/application/beans/vo/VoConducenteUno.strAnniPatente", "index=5"
+    type_text("under26First_bornDay", Chronic.parse("25 year ago tomorrow").strftime("%d"))
+    type_text("under26First_bornMonth", Chronic.parse("25 year ago tomorrow").strftime("%m"))
+    type_text("under26First_bornYear", Chronic.parse("25 year ago tomorrow").strftime("%Y"))
 
-    click_button "SUBMIT"
+    click_option(get('@owner_sex')) #under26First_sex_MASCHIO   -    under26First_sex_FEMMINA
+
+    page.click('under26First_maritalStatus_link')
+    page.click(get("@civil_status"))
+
+    page.click('under26First_profession_link')
+    page.click(get("@job"))
+
+#    store_parameter(:job, page.get_selected_label(@last_element)) if @store_params
+    type_text("under26First_zipCode", get('@owner_zip_code'))
+
+    page.click('under26First_drivingLicencePossess_link')
+    page.click(get("@driving_license_yrs"))
+
+  end
+
+  def page_3
+
+    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
+    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
+
+    type_text("mesePrimaImmatricolazioneVeicolo", get("@matriculation_date_month"))
+    type_text("annoPrimaImmatricolazioneVeicolo", get("@matriculation_date_year"))
+
+    type_text("annoAcquistoVeicolo", get("@purchase_date_year"))
+
+    enhanced_select "marcaVeicolo_link", get("@make")
+#    store_parameter(:make, page.get_selected_label(@last_element)) if @store_params
+#    sleep @sleep*2
+#    page_click @last_element
+    select_model_set_up "modelloVeicolo_link", get("@model")
+#    store_parameter(:model, page.get_selected_label(@last_element)) if @store_params
+    select_model_set_up "alimentazioneVeicolo_link", get("@fuel")
+    select_model_set_up "allestimentoVeicolo_link", get("@set_up")
+    select_model_set_up "tipoAntifurtoVeicolo_link", get("@alarm")
+
+    select_option "usoAbitualeVeicolo_link", get("@habitual_vehicle_use")
+    type_text("kmAnnuiPercorsiVeicolo", get('@km_per_yr'))
+    click_option(get('@vehicle_use'))
+    select_option "ricoveroVeicolo_link", get("@vehicle_shelter")
+
+#    @last_value =~ /un conducente/i ? @one_driver_young = true : @one_driver_young = false
+    type_text("valoreVeicolo", get('@vehicle_value'))
+
+    type_text("numeroVeicoliFamiliariPosseduti_link", get('@family_car'))
+#    click_option(get('@family_members_insured_with_company'))
+
+    click_button "btnContinua"
    	page_wait
 
   end
@@ -191,82 +262,78 @@ class DirectlineSect1 < Test::Unit::TestCase
     @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
     @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
     
-    if get('@insurance_situation') == 'id_radio_bm'
-      select_option "classeProvenienzaAuto", get("@coming_from_bm")
-      select_option "classeAssegnazioneAuto", get("@bm_assigned")
-      click_option(get('@nr_of_yrs_insured_in_the_last_5_yrs'))
-      click_option(get('@claims_total_number'))
-      #      type_text("dataacquistoauto", get('@purchase_date_year'))
-    elsif get('@insurance_situation') == '//input[@name="tipoPolizza" and @value="AU"]'
-      click_option(get('@bersani'))
-      if page.get_attribute("#{@last_element}@value") != "0"
-        #      type_text("dataacquistoauto", get('@purchase_date_year'))
-        type_text("id_scadenzaPolizzaMadre", get('@bersani_policy_expiring_date'))
-        select_option "sinistriRCA12MesiBMAge", get("@nr_of_paid_claims_this_yr")
-        select_option "/qol/application/beans/vo/VoBMAgevolata.strClasseProvPolizzaMadre", get("@bm_assigned")
-      end
-    elsif get('@insurance_situation') == '//input[@name="tipoPolizza" and @value="AN"]'
-      click_option(get('@bersani'))
-      if page.get_attribute("#{@last_element}@value") != "0"
-        #      type_text("dataacquistoauto", get('@purchase_date_year'))
-        type_text("id_scadenzaPolizzaMadre", get('@bersani_policy_expiring_date'))
-        select_option "sinistriRCA12MesiBMAge", get("@nr_of_paid_claims_this_yr")
-        select_option "/qol/application/beans/vo/VoBMAgevolata.strClasseProvPolizzaMadre", get("@bm_assigned")
-      end
-    end
-
-    type_text("annoprimaimmatricolazione", get('@matriculation_date'))
-    is_present?("dataacquistoauto") ? type_text("dataacquistoauto", get('@purchase_date_year')) : nil
-    select_option "select_marca", get("@make")
-    store_parameter(:make, page.get_selected_label(@last_element)) if @store_params
-    sleep @sleep*2
-    page_click @last_element
-    select_model_set_up "select_modelli", get("@model")
-    store_parameter(:model, page.get_selected_label(@last_element)) if @store_params
+#    if get('@insurance_situation') == 'id_radio_bm'
+#      select_option "classeProvenienzaAuto", get("@coming_from_bm")
+#      select_option "classeAssegnazioneAuto", get("@bm_assigned")
+#      click_option(get('@nr_of_yrs_insured_in_the_last_5_yrs'))
+#      click_option(get('@claims_total_number'))
+#      #      type_text("dataacquistoauto", get('@purchase_date_year'))
+#    elsif get('@insurance_situation') == '//input[@name="tipoPolizza" and @value="AU"]'
+#      click_option(get('@bersani'))
+#      if page.get_attribute("#{@last_element}@value") != "0"
+#        #      type_text("dataacquistoauto", get('@purchase_date_year'))
+#        type_text("id_scadenzaPolizzaMadre", get('@bersani_policy_expiring_date'))
+#        select_option "sinistriRCA12MesiBMAge", get("@nr_of_paid_claims_this_yr")
+#        select_option "/qol/application/beans/vo/VoBMAgevolata.strClasseProvPolizzaMadre", get("@bm_assigned")
+#      end
+#    elsif get('@insurance_situation') == '//input[@name="tipoPolizza" and @value="AN"]'
+#      click_option(get('@bersani'))
+#      if page.get_attribute("#{@last_element}@value") != "0"
+#        #      type_text("dataacquistoauto", get('@purchase_date_year'))
+#        type_text("id_scadenzaPolizzaMadre", get('@bersani_policy_expiring_date'))
+#        select_option "sinistriRCA12MesiBMAge", get("@nr_of_paid_claims_this_yr")
+#        select_option "/qol/application/beans/vo/VoBMAgevolata.strClasseProvPolizzaMadre", get("@bm_assigned")
+#      end
+#    end
+#
+#    type_text("annoprimaimmatricolazione", get('@matriculation_date'))
+#    is_present?("dataacquistoauto") ? type_text("dataacquistoauto", get('@purchase_date_year')) : nil
+#
 
 
-    click_button "PROSEGUI"
+    click_button "btnContinua"
+    page_wait
+
+    select_option "fonti_link", get("@how_do_you_know_the_company")
+    click_button "//img[@alt='Vedi il premio']"
     page_wait
 
   end
 
   def page_5
 
-    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
-
-    page_click "allestimentoAuto"
-    #    select_option "allestimentoAuto", get("@set_up")
-    select_model_set_up "allestimentoAuto", get("@set_up")
-    store_parameter(:preparation, page.get_selected_label(@last_element)) if @store_params
-    select_option "UsoPra", get("@habitual_vehicle_use")
-    type_text("KMPercorsi", get('@km_per_yr'))
-    click_option(get('@vehicle_use'))
-
-    click_button "PROSEGUI"
-    page_wait
+#    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
+#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
+#
+#    page_click "allestimentoAuto"
+#    #    select_option "allestimentoAuto", get("@set_up")
+#    select_model_set_up "allestimentoAuto", get("@set_up")
+#    store_parameter(:preparation, page.get_selected_label(@last_element)) if @store_params
+#
+#
+#    click_button "PROSEGUI"
+#    page_wait
 
   end
 
   def page_6
 
-    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
-
-    is_editable?("iCodAntifurto") ? (select_option "iCodAntifurto", get("@alarm"))  : nil
-    select_option "iCodRicoveroNotturno", get("@vehicle_shelter")
-    /^,00/.match(get_value("valoreauto")) ? type_text("valoreauto", get('@vehicle_value')) : nil
-    
-    click_button "SUBMIT"
-    page_wait
+#    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
+#    @logger.info("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE URL: #{page.get_location}"}
+#
+#    is_editable?("iCodAntifurto") ? (select_option "iCodAntifurto", get("@alarm"))  : nil
+#    /^,00/.match(get_value("valoreauto")) ? type_text("valoreauto", get('@vehicle_value')) : nil
+#
+#    click_button "SUBMIT"
+#    page_wait
 
   end
 
   def page_7
 
-    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
-    click_button "AVANTI"
-    page_wait
+#    @logger.warn("#{__FILE__} => #{method_name}") {"#{@kte.company} => CURRENT PAGE TITLE: #{page.get_title.upcase}"}
+#    click_button "AVANTI"
+#    page_wait
 
   end
 
@@ -278,9 +345,9 @@ class DirectlineSect1 < Test::Unit::TestCase
     @last_element, @last_value = "@rca_on_off", get("@rca_on_off")
     case @last_value
     when 'on'
-      select_max("SelM_RCA", get('@public_liability_indemnity_limit'))
-      select_option("SelRateizzazione", get('@instalment')) #ATTENTION! Always use 1 year split
-      select_option("SelFSM_RCA", get('@public_liability_exemption'))
+      select_max("RCA_LIMIT_0_link", get('@public_liability_indemnity_limit'))
+      select_option("frazionamento_link", get('@instalment')) #ATTENTION! Always use 1 year split
+      select_option("RCA_DEDUCTIBLE_0_link", get('@public_liability_exemption'))
 
       uncheck_checkbox(get('@protected_bonus_web_id')) if is_checked?(get('@protected_bonus_web_id'))
       uncheck_checkbox(get('@assistance_web_id')) if is_checked?(get('@assistance_web_id'))
@@ -292,11 +359,11 @@ class DirectlineSect1 < Test::Unit::TestCase
       uncheck_checkbox(get('@natural_events_act_of_vandalism_web_id')) if is_checked?(get('@natural_events_act_of_vandalism_web_id'))
       uncheck_checkbox(get('@theft_fire_coverage_web_id')) if is_checked?(get('@theft_fire_coverage_web_id'))
 
-      click_button "RICALCOLA"
-      sleep @sleep*3
+#      click_button "RICALCOLA"
+#      sleep @sleep*3
 
-      @last_element, @last_value = "@rca_premium_id", get("@rca_premium_id")
-      wait_for_elm @last_value
+#      @last_element, @last_value = "@rca_premium_id", get("@rca_premium_id")
+#      wait_for_elm @last_value
       get_premium(get("@rca_premium_id"))
     else
       raise RangeError, "RC cover cannot be off"
@@ -410,7 +477,7 @@ class DirectlineSect1 < Test::Unit::TestCase
     return present ? page.is_checked(@last_element) : nil
   end
 
-    def is_editable?(name)
+  def is_editable?(name)
 	  present = page.is_editable name
     if present
       @logger.debug("#{__FILE__} => #{method_name}") {"#{@kte.company} => field is editable?: #{present}"}
@@ -419,7 +486,6 @@ class DirectlineSect1 < Test::Unit::TestCase
     end
     return present
   end
-
 
   def page_wait
     page.wait_for_page_to_load site.wait_for_page_to_load
@@ -508,6 +574,25 @@ class DirectlineSect1 < Test::Unit::TestCase
     @logger.debug("#{__FILE__} => #{method_name}") {"#{@kte.company} => PREMIUM = € #{premium.to_s}"}
     assert_not_equal 0, premium.to_i, "Price cannot be equal to zero"
     @kte.rc_premium = premium
+
+  end
+
+  def enhanced_select(p,e)
+
+    a = '//a[@id='+p+']/'+
+    option_array =[]
+    option_array = page.get_text(p)
+    @num_of_el = 0
+    index = 0
+    while index < option_array.length
+      @num_of_el  += 1
+      break if option_array[index] =~ /#{e.gsub("regexpi:","")}/i
+      index += 1
+    end
+
+    @num_of_el == option_array.length ? (@last_value = "label=#{value.split("|")[1]}" ;  page_click @last_element, "#{@last_value}" ) : ( page_select @last_element, "label=#{@last_value}" )
+    assert_equal page.get_selected_label(@last_element), value unless value =~ /regexpi/i unless value =~ /index=/i
+
 
   end
 
