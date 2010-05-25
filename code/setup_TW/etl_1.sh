ba1d305ee0ce7af32169300a8fdfa64502416efa
ugodiiorio <ugodiiorio@kubepartners.com>
2010-05-25 11:59:08 +0200

    Etl1 script TW modified

--- code/setup_TW/etl_1.sh
+++ code/setup_TW/etl_1.sh
@@ -1,30 +1,29 @@
+#!/bin/bash
+EXPECTED_ARGS=2
+E_BADARGS=65
 
-sectors=("sect_1")
-companies=("directline" "genertel" "linear" "quixa" "zurich_connect")
-sectors_x_companies=(
-"Direct Line - Auto" "Genertel - Auto" "Linear - Auto" "Quixa - Auto" "Zurich Connect - Auto" 
-)
+if [ $# -ne $EXPECTED_ARGS ];
+then
+	echo "Pass two arguments on command line (ie. ./etl_1.sh  sect_1 quixa)"
+  	exit $E_BADARGS
+else
+	echo @@@ $1 @@@
+	echo @@@ $2 @@@
+	echo "Starting ..."
+    cd /home/notroot/git/KTE/code/builders/etl/1_generic_profile/script
 
-cd /home/notroot/git/KTE/code/builders/etl/1_generic_profile/script
+    # fieldmapping
+    mysql -u root -pkub01d --database=kte_driver -e "delete from field_mapping"
+    sed s/"sector_placeholder"/$1/g /home/notroot/git/KTE/code/yamls_TW/field_mapping.yml >field_mapping_"$1"
+    ruby build_field_mapping.rb field_mapping_"$1"
+    rm field_mapping_"$1"
+    
 
-
-# fieldmapping
-#mysql -u root -pkub01d --database=kte_driver -e "delete from field_mapping"
-for i in ${sectors[@]}; do
-sed s/"sector_placeholder"/$i/g /home/notroot/git/KTE/code/yamls_TW/field_mapping.yml >temp1
-ruby build_field_mapping.rb temp1
-rm temp1
-done
-
-# profile
-mysql -u root -pkub01d --database=kte_driver -e "delete from insurance_profiles"
-count=0
-for i in ${sectors[@]}; do
-	for j in ${companies[@]}; do
-	sed s/"sector_placeholder"/"$i"/g /home/notroot/git/KTE/code/yamls_TW/profile.yml |sed s/"company_provider_placeholder"/"${sectors_x_companies[$count]}"/g |sed s/"company_group_placeholder"/""$j"_provider_2"/g >temp2
-	ruby build_profile.rb temp2
-	rm temp2
-	let count++
-	done
-done
-
+    # profile
+    DELETE="delete from insurance_profiles  where key_sector_id_str=\""$1"\" and key_companies_group_id_str=\""$2"_provider_2\""
+    mysql -u root -pkub01d --database=kte_driver -e "$DELETE"
+    sed s/"sector_placeholder"/"$1"/g /home/notroot/git/KTE/code/yamls_TW/profile.yml |sed s/"company_provider_placeholder"/"$2"/g |sed s/"company_group_placeholder"/""$2"_provider_2"/g >profile_"$1"_"$2"
+    ruby build_profile.rb profile_"$1"_"$2"
+    rm profile_"$1"_"$2"
+	echo "Finished!"
+fi
