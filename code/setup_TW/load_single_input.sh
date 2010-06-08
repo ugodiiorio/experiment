e6a91749395f36ff060d94acda0ff2f3e00df9b8
ugodiiorio <ugodiiorio@kubepartners.com>
2010-06-08 12:38:39 +0200

    Update script to load the right csv version : 20100502

--- code/setup_TW/load_single_input.sh
+++ code/setup_TW/load_single_input.sh
@@ -14,7 +14,7 @@ else
 	DELETE="delete from provider_2_"$1"_input_file where pol_provider_company_id_str=\""$2"\""
 	mysql -u root -pkub01d --database=kte_driver -e "$DELETE"
 
-	LOAD="load data local infile \"/home/notroot/git/KTE/code/builders/etl/1_generic_profile/csv/prov2_sector_placeholder_company_group_placeholder_20100501_in.csv\" into table provider_2_sector_placeholder_input_file fields terminated by \"#\" escaped by \"\" set pol_provider_company_id_str=\"company_group_placeholder\";"
+	LOAD="load data local infile \"/home/notroot/git/KTE/code/builders/etl/1_generic_profile/csv/prov2_sector_placeholder_company_group_placeholder_20100502_in.csv\" into table provider_2_sector_placeholder_input_file fields terminated by \"#\" escaped by \"\" set pol_provider_company_id_str=\"company_group_placeholder\";"
 	STATEMENT=`echo "$LOAD"|sed -e s/"sector_placeholder"/"$1"/g -e s/"company_group_placeholder"/"$2"/g`
 	mysql -u root -pkub01d --database=kte_driver -e "$STATEMENT "
 fi
